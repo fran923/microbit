69cf0a26d61cf370cd9ea1847bf597483f9136f4
fran923 <anselmifrancisco2008@gmail.com>
2025-06-15 21:41:25 -0300

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,31 @@
+basic.showLeds(`
+    # . . . #
+    # . . . #
+    # # # # #
+    # . . . #
+    # . . . #
+    `)
+basic.pause(1000)
+basic.showLeds(`
+    # # # # #
+    # . . . #
+    # . . . #
+    # . . . #
+    # # # # #
+    `)
+basic.pause(1000)
+basic.showLeds(`
+    # . . . .
+    # . . . .
+    # . . . .
+    # . . . .
+    # # # # .
+    `)
+basic.pause(1000)
+basic.showLeds(`
+    . # # # .
+    # . . . #
+    # # # # #
+    # . . . #
+    # . . . #
+    `)
